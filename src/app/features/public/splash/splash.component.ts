af19117cdd9f0e95921818f10f1d3333ece2d4a0
Mauricio <mauricio.chaves.jr@gmail.com>
2026-05-07 17:23:48 -0300

    feat(mobile): implementar splash publico

--- src/app/features/public/splash/splash.component.ts
+++ src/app/features/public/splash/splash.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonContent, IonSpinner } from '@ionic/angular/standalone';
+
+import { AuthService } from '../../../core/auth/auth.service';
+
+export const SPLASH_DELAY_MS = 1500;
 
 @Component({
   selector: 'sep-splash',
   standalone: true,
-  imports: [IonContent],
-  template: `<ion-content class="ion-padding"><p>SEP</p></ion-content>`,
+  imports: [IonContent, IonSpinner],
+  templateUrl: './splash.component.html',
+  styleUrls: ['./splash.component.scss'],
 })
-export class SplashComponent {}
+export class SplashComponent implements OnInit {
+  private readonly auth = inject(AuthService);
+  private readonly router = inject(Router);
+
+  async ngOnInit(): Promise<void> {
+    await Promise.all([
+      new Promise((resolve) => setTimeout(resolve, SPLASH_DELAY_MS)),
+      this.auth.loadCurrentUser(),
+    ]);
+    await this.router.navigateByUrl('/welcome');
+  }
+}
